93fb0765da2b8337a499941b9daf93a49b1972d0
jyepesa <juank_713@hotmail.com>
2025-10-22 00:04:19 +0200

    remove inline styling and other minor fixes

--- src/main.js
+++ src/main.js
@@ -11,9 +11,9 @@ const lightSlider = document.getElementById("light-slider");
 const jokesArray = [];
 
 async function displayJoke() {
-  saveJokeBtn.style.display = "flex";
+  saveJokeBtn.classList.remove("hidden");
   const joke = await fetchJoke();
-  jokeText.style.fontStyle = "normal";
+  jokeText.classList.remove("italic");
   jokeText.innerText = joke.joke;
   jokeText.setAttribute("data-id", joke.id);
 }
@@ -30,7 +30,7 @@ function saveJoke() {
   jokesArray.push(jokeObj);
   localStorage.setItem("jokes", JSON.stringify(jokesArray));
 
-  jokeAbsence.style.display = "none";
+  jokeAbsence.classList.add("hidden");
   renderSavedJoke(jokeObj);
 }
 
@@ -69,7 +69,7 @@ function renderSavedJoke({ joke, id }) {
 function renderFromStorage() {
   const earlierArr = JSON.parse(localStorage.getItem("jokes"));
   if (earlierArr.length >= 1) {
-    jokeAbsence.style.display = "none";
+    jokeAbsence.classList.add("hidden");
     earlierArr.forEach((joke) => {
       jokesArray.push(joke);
     });
@@ -88,6 +88,10 @@ function renderFromStorage() {
 
 function deleteJoke(id) {
   const jokeIndex = jokesArray.findIndex((joke) => joke.id === id);
+  if (jokeIndex < 0) {
+    alert("Unable to delete the selected joke");
+    return;
+  }
   jokesArray.splice(jokeIndex, 1);
   localStorage.setItem("jokes", JSON.stringify(jokesArray));
 
@@ -99,36 +103,36 @@ function deleteJoke(id) {
     }
   });
   if (savedJokesDiv.innerText === "") {
-    jokeAbsence.style.display = "inline-block";
+    jokeAbsence.classList.remove("hidden");
   }
 }
 
 function toggleLightMode() {
   const lightNodes = document.querySelectorAll("[data-light]");
   if (lightSwitch.checked) {
-    lightNodes.forEach((node) => {
-      document.body.style.backgroundColor = "rgb(240, 236, 245)";
-      node.classList.add("light-mode");
-      lightSlider.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+    document.body.style.backgroundColor = "rgb(240, 236, 245)";
+    lightSlider.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
 </svg>
 `;
+    lightNodes.forEach((node) => {
+      node.classList.add("light-mode");
     });
     localStorage.setItem("mode", "light");
   } else {
-    lightNodes.forEach((node) => {
-      document.body.style.backgroundColor = "rgb(44, 59, 59)";
-      node.classList.remove("light-mode");
-      lightSlider.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+    document.body.style.backgroundColor = "rgb(44, 59, 59)";
+    lightSlider.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
 </svg>
 `;
+    lightNodes.forEach((node) => {
+      node.classList.remove("light-mode");
     });
     localStorage.setItem("mode", "dark");
   }
 }
+renderFromStorage();
 
-document.addEventListener("DOMContentLoaded", renderFromStorage);
 loadJokeBtn.addEventListener("click", displayJoke);
 saveJokeBtn.addEventListener("click", saveJoke);
 lightSwitch.addEventListener("click", toggleLightMode);
