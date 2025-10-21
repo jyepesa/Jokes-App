732d26de5adfd2b7b8673ccbd7c16a1b910f984e
jyepesa <juank_713@hotmail.com>
2025-10-21 14:30:14 +0200

    add dark/light mode

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ const saveJokeBtn = document.getElementById("save-joke");
 const jokeText = document.getElementById("joke-text");
 const savedJokesDiv = document.getElementById("saved-jokes");
 const jokeAbsence = document.getElementById("no-jokes");
+const lightSwitch = document.getElementById("mode-toggle");
+const lightSlider = document.getElementById("light-slider");
 
 const jokesArray = [];
 
 async function displayJoke() {
-  saveJokeBtn.style.display = "inline-block";
+  saveJokeBtn.style.display = "flex";
   const joke = await fetchJoke();
   jokeText.style.fontStyle = "normal";
   jokeText.innerText = joke.joke;
@@ -39,9 +41,16 @@ function renderSavedJoke({ joke, id }) {
 
   jokeNode.classList.add("saved-joke");
   jokeNode.setAttribute("data-id", id);
+  jokeNode.setAttribute("data-light", "yes");
 
   jokeText.innerText = joke;
   jokeText.classList.add("saved-joke__text");
+  jokeText.setAttribute("data-light", "yes");
+
+  if (lightSwitch.checked) {
+    jokeNode.classList.add("light-mode");
+    jokeText.classList.add("light-mode");
+  }
 
   deleteJokeBtn.classList.add("saved-joke--delete");
   deleteJokeBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
@@ -87,6 +96,30 @@ function deleteJoke(id) {
   }
 }
 
+function toggleLightMode() {
+  const lightNodes = document.querySelectorAll("[data-light]");
+  if (lightSwitch.checked) {
+    lightNodes.forEach((node) => {
+      document.body.style.backgroundColor = "rgb(240, 236, 245)";
+      node.classList.add("light-mode");
+      lightSlider.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+</svg>
+`;
+    });
+  } else {
+    lightNodes.forEach((node) => {
+      document.body.style.backgroundColor = "rgb(44, 59, 59)";
+      node.classList.remove("light-mode");
+      lightSlider.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+</svg>
+`;
+    });
+  }
+}
+
 document.addEventListener("DOMContentLoaded", renderFromStorage);
 loadJokeBtn.addEventListener("click", displayJoke);
 saveJokeBtn.addEventListener("click", saveJoke);
+lightSwitch.addEventListener("click", toggleLightMode);
